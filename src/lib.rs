663c6ca4908976b92734e498a0ace8818a7eb7c0
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-06-22 23:39:49 +0530

    autosave src/lib.rs after save

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ struct Node<'a, K, V> {
     next: Link<K, V>,
 }
 
-pub struct Lru<'a, K, V> {
+pub struct Lru<'a, K: EQ+std::hash::HASH, V> {
     capacity: usize,
     size: usize,
     store: Mutex<HashMap<K, V>>,
