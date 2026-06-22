fb6504b8bb969103eb34c233c8de8412a6113c01
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-06-23 00:35:27 +0530

    autosave src/lib.rs after save

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ impl<K: Ord, V> lru<K, V> {
         Lru{ capacity }
     }
 
-    fn headInsert(&mut self, k:K, v: usize) {
+    fn headInsert(&mut self, k: K, v: V) {
 
         self.nodes.push(Node.new(k, v))
 
