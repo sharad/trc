c90a9260a5fe7030e572f1039f4ff93ca77b64ec
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-06-23 00:20:57 +0530

    autosave src/lib.rs after save

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ impl<K: Ord, V> lru<K, V> {
     fn tailRelease(&mut self) {
         match self.tail {
             0 => {}
-            Some(Arc( Ref(Node { tail }) )) => {
+             => {
                 tail = tail.prev;
             }
         }
